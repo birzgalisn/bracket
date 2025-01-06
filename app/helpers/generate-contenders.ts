802bb284fdc686cac7effdf1cc463161a853f54d
Niks Birzgalis <hello@niksbirzgalis.com>
2025-01-06 16:56:44 +0200

    fix: change default contender name

--- app/helpers/generate-contenders.ts
+++ app/helpers/generate-contenders.ts
@@ -10,7 +10,7 @@ export default function generateContenders(bracketSize = initialSize) {
   for (let id = 0; id < bracketSize * 2; id++) {
     const no = `${id + 1}`;
     const flag = flags[generateNumber(flags.length - 1)];
-    const name = `Team ${no}`;
+    const name = `Contender ${no}`;
     const scores = contenderDefaults.scores;
 
     contenders.push({ id: `contender-${id}`, no, flag, name, scores });
